27f0cdde1270dd959b35bbe210e3c878d4eada2e
5hes <553919500@qq.com>
2024-12-15 15:58:46 +0800

    Update HintingChecker.sh

--- HintingChecker.sh
+++ HintingChecker.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 检查是否提供了字体文件目录
-if [ -z "\$1" ]; then
-    echo "用法: \$0 <字体文件目录>"
-    exit 1
-fi
+# if [ -z "\$1" ]; then
+#     echo "用法: \$0 <字体文件目录>"
+#     exit 1
+# fi
 
 # 遍历指定目录下的所有字体文件
-for font_file in "\$1"/*; do
+# for font_file in "\$1"/*; do
     # 检查文件是否为字体文件
-    if [[ $font_file == *.ttf || $font_file == *.otf ]]; then
+#     if [[ $font_file == *.ttf || $font_file == *.otf ]]; then
         # 使用 ttx 导出字体信息
-        ttx -o temp.ttx "$font_file" >/dev/null 2>&1
+#         ttx -o temp.ttx "$font_file" >/dev/null 2>&1
 
         # 检查是否存在 hinting 信息
         if grep -q "<hints>" temp.ttx; then
@@ -22,7 +22,7 @@ for font_file in "\$1"/*; do
         fi
 
         # 删除临时文件
-        rm temp.ttx
-    fi
-done
+#         rm temp.ttx
+#     fi
+# done
 # 啊啊啊啊啊啊啊啊啊啊
